8f5fb860c440c0c9552ffda65a9b44ab5e702892
tam kien duong <tk@renard.ninja>
2025-01-31 11:08:36 +0100

    fix a minor typo

--- src/ollama.tsx
+++ src/ollama.tsx
@@ -277,8 +277,8 @@ export async function askAI(prompt: string, context: string) {
 
 export async function convertToFlashCard(uuid: string, blockContent: string) {
   try {
-    const questionBlock = await logseq.Editor.insertBlock(uuid, "⌛Genearting question....", { before: false })
-    const answerBlock = await logseq.Editor.insertBlock(questionBlock!.uuid, "⌛Genearting answer....", { before: false })
+    const questionBlock = await logseq.Editor.insertBlock(uuid, "⌛Generating question....", { before: false })
+    const answerBlock = await logseq.Editor.insertBlock(questionBlock!.uuid, "⌛Generating answer....", { before: false })
     const question = await promptLLM(`Create a question for a flashcard. Provide the question only. Here is the knowledge to check:\n ${blockContent}`)
     const answer = await promptLLM(`Given the question ${question} and the context of ${blockContent} What is the answer? be as brief as possible and provide the answer only.`)
     await logseq.Editor.updateBlock(questionBlock!.uuid, `${question} #card`)
